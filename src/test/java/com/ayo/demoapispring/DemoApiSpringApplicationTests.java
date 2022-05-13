f8cf845f0d53f19d19cab5d689cfc7049726df69
unknown <ayo.agusyulianto@gmail.com>
2022-05-13 12:45:54 +0700

    commit pertama

---
+++ src/test/java/com/ayo/demoapispring/DemoApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayo.demoapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
